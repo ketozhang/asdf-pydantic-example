07125b71ddc6f2a86cc9ed47982423564a4ba108
Keto D. Zhang <keto.zhang@gmail.com>
2024-11-14 16:54:13 -0800

    Add shapes package

---
+++ shapes/integration.py
@@ -0,0 +1,28 @@
+import asdf
+from asdf import AsdfFile
+from astropy import units as u
+from shapes.circle import Circle
+
+import asdf
+import yaml
+from asdf.extension import Extension, TagDefinition
+from asdf_pydantic import AsdfPydanticConverter
+
+AsdfPydanticConverter.add_models(Circle)
+
+
+class ShapesExtension(Extension):
+    extension_uri = "asdf://asdf-pydantic/examples/extensions/shapes-1.0.0"
+    converters = [AsdfPydanticConverter()]
+    tags = [
+        TagDefinition(Circle._tag, schema_uris=[Circle._tag + "/schema"]),
+    ]
+
+
+def get_extensions():
+    return [ShapesExtension()]
+
+
+def get_resource_mappings():
+    schema = yaml.safe_load(Circle.model_asdf_schema())
+    return [{schema["id"]: yaml.dump(schema)}]

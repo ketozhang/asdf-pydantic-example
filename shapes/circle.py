a32532792893d6220db875abf2897812c343760c
Keto D. Zhang <keto.zhang@gmail.com>
2024-11-15 11:54:13 -0800

    Use tag: for schema reference in Circle radius field

--- shapes/circle.py
+++ shapes/circle.py
@@ -1,13 +1,10 @@
 from typing import Annotated
 
-from asdf_pydantic import AsdfPydanticModel
+from asdf_pydantic import AsdfPydanticConverter, AsdfPydanticModel
 from asdf_pydantic.schema import AsdfTag
 from astropy import units as u
-from asdf_pydantic import AsdfPydanticConverter
 
 
 class Circle(AsdfPydanticModel):
     _tag = "asdf://asdf-pydantic/examples/tags/circle-1.0.0"
-    radius: Annotated[
-        u.Quantity[u.m], AsdfTag("http://stsci.edu/schemas/asdf/unit/quantity-1.2.0")
-    ]
+    radius: Annotated[u.Quantity[u.m], AsdfTag("tag:stsci.edu:asdf/unit/quantity-1.*")]

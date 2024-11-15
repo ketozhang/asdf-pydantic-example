bf6e66b60ef002cda2984d64ceeb5349dcc36774
Keto D. Zhang <keto.zhang@gmail.com>
2024-11-14 17:25:44 -0800

    Add test schema exists"

--- test.py
+++ test.py
@@ -7,10 +7,16 @@ from astropy import units as u
 from shapes.circle import Circle
 
 
+def test_schema_exists():
+    from asdf.schema import load_schema
+
+    assert load_schema(Circle._tag + "/schema")
+
+
 def test_write_asdf(tmp_path):
     af = AsdfFile({"data": Circle(radius=1 * u.m)})
     af.write_to(tmp_path / "circle.asdf")
 
-    with asdf.open("circle.asdf") as af:
+    with asdf.open(tmp_path / "circle.asdf") as af:
         af.info()
         logging.info(af.tree["data"])
